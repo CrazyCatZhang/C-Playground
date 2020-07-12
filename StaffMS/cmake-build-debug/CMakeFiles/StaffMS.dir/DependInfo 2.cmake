6007a907cc54e11bf25044451ac14f262c598cf3
CatZhang <CrazyCatZhang@outlook.com>
2020-07-12 14:53:59 +0800

    Update

---
+++ StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/DependInfo 2.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/zhangtao/CODE/StaffMS/Source/Boss.cpp" "/Users/zhangtao/CODE/StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/Source/Boss.cpp.o"
+  "/Users/zhangtao/CODE/StaffMS/Source/Employee.cpp" "/Users/zhangtao/CODE/StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/Source/Employee.cpp.o"
+  "/Users/zhangtao/CODE/StaffMS/Source/Manager.cpp" "/Users/zhangtao/CODE/StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/Source/Manager.cpp.o"
+  "/Users/zhangtao/CODE/StaffMS/Source/WorkerManagement.cpp" "/Users/zhangtao/CODE/StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/Source/WorkerManagement.cpp.o"
+  "/Users/zhangtao/CODE/StaffMS/main.cpp" "/Users/zhangtao/CODE/StaffMS/cmake-build-debug/CMakeFiles/StaffMS.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
